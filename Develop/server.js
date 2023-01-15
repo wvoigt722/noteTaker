e28ec51402452e55d5d9f93e5404fd5b532bd1f3
William Voigt <wvoigt@Williams-Air.lan1>
2023-01-15 13:37:50 -0800

    fixed routing issue in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -13,13 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 app.use(express.static("public"));
 
-//homepage
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
 //notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+//homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () => console.log("Now listening"));
