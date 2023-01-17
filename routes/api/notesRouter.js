aeae803d04810f67495783e7df0f105c1581f17e
William Voigt <wvoigt@Williams-Air.lan1>
2023-01-17 10:00:55 -0800

    added id to the db so we could pull from it

--- routes/api/notesRouter.js
+++ routes/api/notesRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { v4: uuidv4 } = require("uuid");
+const { v4 } = require("uuid");
 const {
   readFromFile,
   readAndAppend,
@@ -7,18 +7,22 @@ const {
 } = require("../../helpers/fsUtil");
 
 router.get("/", (req, res) => {
-  readFromFile("./db/noteDb.json").then((data) => res.json(JSON.parse(data)));
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 router.post("/", (req, res) => {
   console.log(req.body);
-  readAndAppend(req.body, "./db/noteDB.json");
+  const newNote = {
+    ...req.body,
+    id: v4(),
+  };
+  readAndAppend(newNote, "./db/db.json");
   res.json(`Note Added`);
 });
 
 router.delete("/:note_id", (req, res) => {
   const noteId = req.params.note_id;
-  readFromFile("./db/noteDb.json")
+  readFromFile("./db/db.json")
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all tips except the one with the ID provided in the URL
